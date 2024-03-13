3e3c5339e004caf0973c50f243cc32a65ebc074c
Shaaf <shaaf.m.shahzad@gmail.com>
2024-03-13 12:57:15 -0400

    Updated pt2

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,18 +5,13 @@ export default function Resume() {
 		<div className="w-full h-full flex flex-col justify-center items-center text-center">
 			<div style={{ width: "100%", height: "90vh", overflow: "hidden" }}>
 				<iframe
-					src="/ShaafShahzadResume2024.pdf"
 					title="Resume"
 					width="100%"
 					height="100%"
 					style={{ border: "none", overflow: "hidden" }}
 				></iframe>
 			</div>
-			<a
-				href="/ShaafShahzadResume2024.pdf"
-				download="ShaafShahzadResume2024.pdf"
-				className="mt-4 transition duration-200 ease-in-out hover:text-[#f8c8dc] hover:-translate-y-1"
-			>
+			<a className="mt-4 transition duration-200 ease-in-out hover:text-[#f8c8dc] hover:-translate-y-1">
 				Download Resume
 			</a>
 		</div>

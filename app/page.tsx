0fb1827e0eb7b8a9eb903cca318b07f90489583c
Shaaf <shaaf.m.shahzad@gmail.com>
2024-06-09 04:22:13 -0400

    Changed tool glow

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
 import React from "react";
 
 export default function Home() {
-	return (
-		<div className="w-full h-full flex flex-col justify-center items-center text-center text-[12px] md:text-md lg:text-lg">
-			<h1>Hey, I'm Shaaf! 👋😄</h1>
-			<br />
-			<p>
-				I'm a second-year Computer Engineering student 💻🎓 at Toronto
-				Metropolitan University, deeply passionate about the vast fields
-				of web development 🌐, AI 🧠, and machine learning 📊
-			</p>
-			<br />
-			<p>
-				When I'm not at the computer, I'm either dozing off 😴 amidst a
-				sea of circuits during ELE lectures ⚡, or eagerly expanding my
-				knowledge and exploring new insights that benefit and fuel my
-				passion for technology 🔥💡
-			</p>
-			<br />
-			<p>
-				If you're interested in discussing technology, projects, or just
-				want to say hi, feel free to reach out to me at{" "}
-				<a href="https://mail.google.com/mail/?view=cm&fs=1&to=shaaf.m.shahzad@gmail.com">
-					<u>shaaf.m.shahzad@gmail.com</u>
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center text-center text-[12px] md:text-md lg:text-lg">
+      <h1>Hey, I'm Shaaf! 👋😄</h1>
+      <br />
+      <p>
+        I'm a second-year Computer Engineering student 💻🎓 at Toronto
+        Metropolitan University, deeply passionate about the vast fields of web
+        development 🌐, AI 🧠, and machine learning 📊
+      </p>
+      <br />
+      <p>
+        When I'm not at the computer, I'm either dozing off 😴 amidst a sea of
+        circuits during ELE lectures ⚡, or eagerly expanding my knowledge and
+        exploring new insights that benefit and fuel my passion for technology
+        🔥💡
+      </p>
+      <br />
+      <p>
+        If you're interested in discussing technology, projects, or just want to
+        say hi, feel free to reach out to me at{" "}
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=shaaf.m.shahzad@gmail.com">
+          <u>shaaf.m.shahzad@gmail.com</u>
+        </a>
+      </p>
+    </div>
+  );
 }
--- components/Project.tsx
+++ components/Project.tsx
@@ -4,62 +4,58 @@ import Link from "next/link";
 import { tools as toolColors } from "../lib/tools";
 
 interface ProjectProps {
-	name: string;
-	description: string;
-	previewImage: string;
-	tools: string[];
-	linkTo: string;
+  name: string;
+  description: string;
+  previewImage: string;
+  tools: string[];
+  linkTo: string;
 }
 
 const Project = ({
-	name,
-	description,
-	previewImage,
-	tools,
-	linkTo,
+  name,
+  description,
+  previewImage,
+  tools,
+  linkTo,
 }: ProjectProps) => {
-	return (
-		<Link
-			href={linkTo}
-			target="_blank"
-			rel="noopener noreferrer"
-			className="no-underline"
-		>
-			<div className="w-full mx-auto bg-gray-800 bg-opacity-50 rounded-lg overflow-hidden shadow-lg my-4 flex flex-row group cursor-pointer hover:shadow-2xl transition duration-200">
-				<div className="w-full p-4 flex flex-col md:flex-row justify-between space-y-2 md:space-x-10">
-					<div className="md:w-2/3">
-						<h2 className="text-sm font-bold flex items-center group-hover:text-[#f8c8dc] transition duration-200">
-							{name}
-							<ArrowTopRightIcon className="ml-2 w-5 h-5 group-hover:text-[#f8c8dc] group-hover:-translate-y-1 group-hover:translate-x-1 transition duration-200" />
-						</h2>
-						<p className="text-xs mb-2 whitespace-pre-line">
-							{description}
-						</p>
-					</div>
-					<div className="flex flex-wrap md:w-[47%] items-start">
-						{tools.map((tool) => {
-							const toolStyle =
-								toolColors[tool as keyof typeof toolColors];
-							return (
-								<span
-									key={tool}
-									className="rounded-full px-3 py-1 text-[10px] sm:text-xs font-semibold mr-2 mb-2 h-fit transition duration-300 transform hover:-translate-y-1"
-									style={{
-										backgroundColor:
-											toolStyle?.backgroundColor,
-										color: toolStyle?.color,
-										boxShadow: `0 0 8px ${toolStyle?.backgroundColor}`,
-									}}
-								>
-									{tool}
-								</span>
-							);
-						})}
-					</div>
-				</div>
-			</div>
-		</Link>
-	);
+  return (
+    <Link
+      href={linkTo}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="no-underline"
+    >
+      <div className="w-full mx-auto bg-gray-800 bg-opacity-50 rounded-lg overflow-hidden shadow-lg my-4 flex flex-row group cursor-pointer hover:shadow-2xl transition duration-200">
+        <div className="w-full p-4 flex flex-col md:flex-row justify-between space-y-2 md:space-x-10">
+          <div className="md:w-2/3">
+            <h2 className="text-sm font-bold flex items-center group-hover:text-[#f8c8dc] transition duration-200">
+              {name}
+              <ArrowTopRightIcon className="ml-2 w-5 h-5 group-hover:text-[#f8c8dc] group-hover:-translate-y-1 group-hover:translate-x-1 transition duration-200" />
+            </h2>
+            <p className="text-xs mb-2 whitespace-pre-line">{description}</p>
+          </div>
+          <div className="flex flex-wrap md:w-[47%] items-start">
+            {tools.map((tool) => {
+              const toolStyle = toolColors[tool as keyof typeof toolColors];
+              return (
+                <span
+                  key={tool}
+                  className="rounded-full px-3 py-1 text-[10px] sm:text-xs font-semibold mr-2 mb-2 h-fit transition duration-300 transform hover:-translate-y-1"
+                  style={{
+                    backgroundColor: toolStyle?.backgroundColor,
+                    color: toolStyle?.color,
+                    boxShadow: `0 0 6px ${toolStyle?.backgroundColor}`,
+                  }}
+                >
+                  {tool}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default Project;
